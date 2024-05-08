8d49a356ed4bd2b479dfc5a6dd27b7265d5fab98
FabianLorenz <fabian.ramelsberger@salmi.de>
2024-05-08 20:04:41 +0200

    feature: added new condition and refactored

---
+++ Assets/TickTacToeGame/Scripts/GrabberIsLocalPlayerCondition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using FREngine.Events;
+using Fusion;
+using Fusion.XR.Shared.Grabbing.NetworkHandColliderBased;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GrabberIsLocalPlayerCondition : ICondition
+{
+    public bool Execute(Transform emitter)
+    {
+        var grabbable = emitter.GetComponentInParent<NetworkHandColliderGrabbable>();
+        PlayerRef localPlayer = PlayerManagerScript.Instance.Runner.LocalPlayer;
+        return localPlayer == grabbable.Object.StateAuthority;
+    }
+}
